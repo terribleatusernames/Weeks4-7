094614f4d5a7b1b1363c42d03935ee2e6a98e49f
mintsopo <mintsopo@OA-S138-11>
2026-02-04 14:52:44 -0500

    Coding Gym

---
+++ Assets/Scripts/On Click Destroy.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class OnClickDestroy : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public bool test = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        worldMousePosition.z = 0;
+        bool isHovered = spriteRenderer.bounds.Contains(worldMousePosition);
+
+        if (isHovered)
+        {
+            test = true;
+        } else {
+            test = false;
+        }
+
+        if (test && Input.GetMouseButtonDown(0))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
