b3463942599da0300fda786c9d01cda5c6238ce9
mintsopo <mintsopo@OA-S318-05>
2026-02-09 20:11:24 -0500

    Coding Gym Solution

--- Assets/Scripts/Text Pos.cs
+++ Assets/Scripts/Text Pos.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 
 public class TextPos : MonoBehaviour
 {
-    public Vector3 textPos;
+    public Vector3 textPos = new(0, 0, 1);
+    public Vector3 worldPos;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +15,7 @@ public class TextPos : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        worldPos = Camera.main.WorldToScreenPoint(transform.position);
+        transform.position = worldPos;
     }
 }
