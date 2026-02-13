7cf73d0fb37f9134ce73e84a18148ab2478ae8d4
mintsopo <mintsopo@OA-S141-22>
2026-02-13 16:13:30 -0500

    Week 6 Inclass

---
+++ Assets/Scripts/Zoo.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections.Generic;
+public class Zoo : MonoBehaviour
+{
+    public List<string> animals;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animals.Add("snake");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
